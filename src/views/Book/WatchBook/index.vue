5d4539391e19bff350b4e4bfe40e05e48e90c922
buiyenlinh <buiyenlinh1406@gmail.com>
2021-12-13 09:49:21 +0700

    commit book

--- src/views/Book/WatchBook/index.vue
+++ src/views/Book/WatchBook/index.vue
@@ -49,14 +49,14 @@ export default defineComponent({
         <div class="modal-body">
           <div class="row">
             <div class="col-md-5 col-xs-12 col-12">
-              <div class="cover-image">
-                <img v-if="book?.cover_image" :src="URL_IMAGE + book?.cover_image" alt="Ảnh bìa sách" style="width: 100%; height: 400px; object-fit: cover">
+              <div class="cover-image mb-3">
+                <img v-if="book?.cover_image" :src="URL_IMAGE + book?.cover_image" alt="Ảnh bìa sách">
                 <span  v-else>Ảnh bìa sách</span>
               </div>
             </div>
             <div class="col-md-7 col-xs-12 col-12">
               <div class="info">
-                <h3>{{ book?.title }}</h3>
+                <h3 class="text-set-01">{{ book?.title }}</h3>
                 <p><b>Loại sách:</b> {{ book?.category?.name }}</p>
                 <p><b>Ngôn ngữ:</b> {{ book?.language }}</p>
                 <p class="mb-2">
@@ -67,31 +67,33 @@ export default defineComponent({
                 <p><b>Thời gian phát hành:</b> {{ formatDate(book?.release_time) }}</p>
               </div>
             </div>
-          </div>
-
-          <div class="content pb-3">
-            <div class="mp3">
-              <div class="mt-3 mb-2"><b>Mp3</b></div>
-              <audio controls v-if="book?.mp3">
-                <source :src="URL_IMAGE + book?.mp3" type="audio/mpeg">
-              </audio>
-            </div>
-            <div class="mt-3 mb-2"><b>Nội dung</b></div>
-            <div id="accordion">
-              <div class="card" v-for="(item, index) in book?.content" :key="index">
-                <div class="card-header">
-                  <a class="card-link" data-toggle="collapse" :href="'#collaps' + item?.id">
-                    {{ item?.title }}
-                  </a>
+            <div class="col-md-12 col-sm-12 col-12">
+              <div class="content pb-3">
+                <div class="mp3">
+                  <div class="mb-2"><b>Mp3</b></div>
+                  <audio controls v-if="book?.mp3">
+                    <source :src="URL_IMAGE + book?.mp3" type="audio/mpeg">
+                  </audio>
                 </div>
-                <div :id="'collaps' + item?.id" :class="[index == 0 ? 'show' : '', 'collapse']" data-parent="#accordion">
-                  <div class="card-body">
-                    {{ item?.content }}
+                <div class="mt-3 mb-2"><b>Nội dung</b></div>
+                <div id="accordion">
+                  <div class="card" v-for="(item, index) in book?.content" :key="index">
+                    <div class="card-header">
+                      <a class="card-link" data-toggle="collapse" :href="'#collaps' + item?.id">
+                        {{ item?.title }}
+                      </a>
+                    </div>
+                    <div :id="'collaps' + item?.id" :class="[index == 0 ? 'show' : '', 'collapse']" data-parent="#accordion">
+                      <div class="card-body">
+                        {{ item?.content }}
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          
         </div>
 
         <div class="modal-footer">
@@ -108,7 +110,26 @@ export default defineComponent({
 
 
 <style lang="scss" scoped>
+.cover-image {
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+  border: 1px solid #252f3b;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    transform: scale(1);
+    transition: all ease-in-out 0.7s;
+  }
+}
+
+.cover-image:hover img {
+  transform: scale(1.2);
+}
+
 .card-header a {
   display: block;
 }
+
 </style>
