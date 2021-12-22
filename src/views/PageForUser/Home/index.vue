697078ca12dbec5ace80bb3af0523003f6573e9f
buiyenlinh <buiyenlinh1406@gmail.com>
2021-12-22 09:40:53 +0700

    commit

--- src/views/PageForUser/Home/index.vue
+++ src/views/PageForUser/Home/index.vue
@@ -1,73 +1,68 @@
-<script>
+<script lang="ts">
 import { defineComponent, onMounted, provide, ref } from 'vue'
 import UsePageForUser from '../UsePageForUser';
-import 'swiper/swiper-bundle.min.css'
-import SwiperCore, {
-  Autoplay,Pagination,Navigation
-} from 'swiper';
-SwiperCore.use([Autoplay,Pagination,Navigation]);
-// import "swiper/css/bundle";
-// import "swiper/css";
+
+
 import { Icon } from "@iconify/vue";
 export default defineComponent({
   name: 'Home',
   components: {
     Icon
   },
-  // setup() {
-  //   const { getCategory, getNewBookList } = UsePageForUser();
-  //   const categoryList = ref();
-  //   const newBookList = ref();
-  //   onMounted(() => {
-  //     getCategory().then(response => {
-  //       categoryList.value = response?.data?.data;
-  //     })
+  setup() {
+    const { getCategory, getNewBookList } = UsePageForUser();
+    const categoryList = ref();
+    const newBookList = ref();
+    onMounted(() => {
+      getCategory().then(response => {
+        categoryList.value = response?.data?.data;
+      })
 
-  //     getNewBookList(8).then(response => {
-  //       newBookList.value = response?.data?.data;
-  //       console.log(newBookList.value);
-  //     })
-  //   });
+      getNewBookList(8).then(response => {
+        newBookList.value = response?.data?.data;
+        console.log(newBookList.value);
+      })
+    });
 
-  //   return {
-  //     categoryList,
-  //     newBookList
-  //   }
-  // },
-  // methods: {
-  //   createString(str: string) {
-  //     var AccentsMap = [
-  //       "aàảãáạăằẳẵắặâầẩẫấậ",
-  //       "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
-  //       "dđ", "DĐ",
-  //       "eèẻẽéẹêềểễếệ",
-  //       "EÈẺẼÉẸÊỀỂỄẾỆ",
-  //       "iìỉĩíị",
-  //       "IÌỈĨÍỊ",
-  //       "oòỏõóọôồổỗốộơờởỡớợ",
-  //       "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
-  //       "uùủũúụưừửữứự",
-  //       "UÙỦŨÚỤƯỪỬỮỨỰ",
-  //       "yỳỷỹýỵ",
-  //       "YỲỶỸÝỴ"    
-  //     ];
-  //     for (let i=0; i<AccentsMap.length; i++) {
-  //       let re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
-  //       let char = AccentsMap[i][0];
-  //       str = str.replace(re, char);
-  //     }
-  //     str = str.trim();
-  //     str = str.replace(/\s+/g, '-').toLowerCase();
-  //     str = str.replace(/[[]&#,+()$~%.'":*?<>{}]/g, '');
-  //     return str;
-  //   }
-  // }
+    return {
+      categoryList,
+      newBookList
+    }
+  },
+  methods: {
+    createString(str: string) {
+      var AccentsMap = [
+        "aàảãáạăằẳẵắặâầẩẫấậ",
+        "AÀẢÃÁẠĂẰẲẴẮẶÂẦẨẪẤẬ",
+        "dđ", "DĐ",
+        "eèẻẽéẹêềểễếệ",
+        "EÈẺẼÉẸÊỀỂỄẾỆ",
+        "iìỉĩíị",
+        "IÌỈĨÍỊ",
+        "oòỏõóọôồổỗốộơờởỡớợ",
+        "OÒỎÕÓỌÔỒỔỖỐỘƠỜỞỠỚỢ",
+        "uùủũúụưừửữứự",
+        "UÙỦŨÚỤƯỪỬỮỨỰ",
+        "yỳỷỹýỵ",
+        "YỲỶỸÝỴ"    
+      ];
+      for (let i=0; i<AccentsMap.length; i++) {
+        let re = new RegExp('[' + AccentsMap[i].substr(1) + ']', 'g');
+        let char = AccentsMap[i][0];
+        str = str.replace(re, char);
+      }
+      str = str.trim();
+      str = str.replace(/\s+/g, '-').toLowerCase();
+      str = str.replace(/[[]&#,+()$~%.'":*?<>{}]/g, '');
+      return str;
+    }
+  }
 })
 </script>
 
 <template>
   <div id="home">
-    <!-- <div class="home">
+    <div class="home">
       <div class="home-bg">
         <div class="img">
           <img src="@/assets/images/background-header-01.jpg" alt="">
@@ -96,22 +91,12 @@ export default defineComponent({
       <div class="home-book-new">
         <div class="container">
           <h3 class="title-block">Sách mới</h3>
-          
           <div class="text-center">
             <button class="btn btn-primary btn-sm">Xem thêm</button>
           </div>
         </div>
       </div>
-    </div> -->
-  
-  <swiper :spaceBetween="30" :centeredSlides="true" :autoplay='{
-            "delay": 2500,
-            "disableOnInteraction": false
-          }' :pagination='{
-            "clickable": true
-          }' :navigation="true" class="mySwiper">
-            <swiper-slide>Slide 1</swiper-slide><swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide><swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide><swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide><swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
-          </swiper>
+    </div>
   </div>
 
 </template>
